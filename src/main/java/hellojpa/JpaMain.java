5f67b030da16ce1db8633c7155ad6af6bf829224
hajs <dev.hajs@gmail.com>
2021-07-01 17:27:38 +0900

    플러시 - 플러시 방법(직접호출, JPQL) 학습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,35 +17,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 동일성 보장 확인
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-//            System.out.println("result = " + (findMember1 == findMember2));
+            // flush 직접 호출 -> 트랜잭션 종료 전에 query 가 나가는지 확인
+//            Member member = new Member(200L, "member200");
+//            em.persist(member);
+//            em.flush();
+//            System.out.println("===========");
 
-            // 쓰기 지연 확인
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("=============");
-//             tx.commit();
-
-            // dirty checking 확인
-//            Member findMember = em.find(Member.class, 150L);
-//            findMember.setName("ZZZZZ");
-//            System.out.println("=============");
-
-            // entity 삭제 (내가 궁금해서 짠 코드! -> 삭제는 RDBMS 에서 auto commit 이 되는 작업이다. JPA 에서는 어떻게 될까? => 트랜잭션이 끝날 때 작업한다! (전제조건을 보장하는구나))
-//            Member findMember = em.find(Member.class, 160L);
-//            em.remove(findMember);
-//            System.out.println("=============");
-
-            // entity 추가와 삭제 (내가 궁금해서 짠 코드! -> 한 트랜잭션 안에서 객체를 생성하고 제거하면 1차 캐시 내에서만 핸들링 되는걸까? => 쓰기 지연 SQL 저장소에 insert, delete query 가 적재되었고 실행됐다!)
-            Member memberHa = new Member(1000L, "하");
-            em.persist(memberHa);
-            System.out.println("=============");
-            em.remove(memberHa);
-            System.out.println("=============");
+            // 중간에 JPQL 쿼리 실행하는 경우 -> 잘못된 동작을 방지하기 위해 플러시가 자동 호출됩니다. 따라서 memberA, memberB, memberC 는 조회가 됩니다.
+            Member memberA = new Member(3L, "memberA");
+            Member memberB = new Member(4L, "memberB");
+            Member memberC = new Member(5L, "memberC");
+            em.persist(memberA);
+            em.persist(memberB);
+            em.persist(memberC);
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+            for (Member member : members) {
+                System.out.println("member.id = " + member.getId() + ", member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
