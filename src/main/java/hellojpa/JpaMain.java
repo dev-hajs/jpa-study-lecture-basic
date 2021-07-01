67bc8c7ed5af8186eab0fb0cae7b5cc8931f6ec3
hajs <dev.hajs@gmail.com>
2021-07-01 17:36:02 +0900

    준영속 상태 - 특정 Entity 를 준영속 상태로 만드는 방법 학습 - 영속성 컨텍스트를 통째로 날리는 방법 학습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,23 +17,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // flush 직접 호출 -> 트랜잭션 종료 전에 query 가 나가는지 확인
-//            Member member = new Member(200L, "member200");
-//            em.persist(member);
-//            em.flush();
+            // 영속
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("AAAAA");
+
+            // 준영속 => findMember 객체를 준영속 상태로 만들었으므로 update query 가 날라가지 않는다.
+//            em.detach(findMember);
 //            System.out.println("===========");
 
-            // 중간에 JPQL 쿼리 실행하는 경우 -> 잘못된 동작을 방지하기 위해 플러시가 자동 호출됩니다. 따라서 memberA, memberB, memberC 는 조회가 됩니다.
-            Member memberA = new Member(3L, "memberA");
-            Member memberB = new Member(4L, "memberB");
-            Member memberC = new Member(5L, "memberC");
-            em.persist(memberA);
-            em.persist(memberB);
-            em.persist(memberC);
-            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
-            for (Member member : members) {
-                System.out.println("member.id = " + member.getId() + ", member.name = " + member.getName());
-            }
+            // 준영속 => 영속성 컨텍스트를 통으로 날렸으므로 이때도 update query 가 날라가지 않는다.
+            em.clear();
+            Member fineMember2 = em.find(Member.class, 150L);
+            System.out.println("fineMember2.id = " + fineMember2.getId());
+            System.out.println("fineMember2.name = " + fineMember2.getName());
 
             tx.commit();
         } catch (Exception e) {
