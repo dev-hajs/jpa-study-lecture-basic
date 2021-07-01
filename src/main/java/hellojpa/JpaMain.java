da96792672167cb5e861ea90fd03a04f69d5a570
hajs <dev.hajs@gmail.com>
2021-07-01 17:08:34 +0900

    영속성 컨텍스트2 - 동일성 보장 확인 - 쓰기 지연 확인 - 변경 감지 확인 - 내가 궁금했던, 객체 삭제 동작 방식 확인 - 내가 궁금했던, 한 트랜잭션 내 객체 생성과 삭제 동작 방식 확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,35 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+            // 동일성 보장 확인
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//            System.out.println("result = " + (findMember1 == findMember2));
 
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
+            // 쓰기 지연 확인
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("=============");
+//             tx.commit();
+
+            // dirty checking 확인
+//            Member findMember = em.find(Member.class, 150L);
+//            findMember.setName("ZZZZZ");
+//            System.out.println("=============");
+
+            // entity 삭제 (내가 궁금해서 짠 코드! -> 삭제는 RDBMS 에서 auto commit 이 되는 작업이다. JPA 에서는 어떻게 될까? => 트랜잭션이 끝날 때 작업한다! (전제조건을 보장하는구나))
+//            Member findMember = em.find(Member.class, 160L);
+//            em.remove(findMember);
+//            System.out.println("=============");
+
+            // entity 추가와 삭제 (내가 궁금해서 짠 코드! -> 한 트랜잭션 안에서 객체를 생성하고 제거하면 1차 캐시 내에서만 핸들링 되는걸까? => 쓰기 지연 SQL 저장소에 insert, delete query 가 적재되었고 실행됐다!)
+            Member memberHa = new Member(1000L, "하");
+            em.persist(memberHa);
+            System.out.println("=============");
+            em.remove(memberHa);
+            System.out.println("=============");
 
             tx.commit();
         } catch (Exception e) {
