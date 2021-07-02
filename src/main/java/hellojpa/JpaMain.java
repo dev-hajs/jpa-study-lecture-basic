a54f49ed41ec6b93772069734b43ca2080bc4bf2
hajs <dev.hajs@gmail.com>
2021-07-02 15:45:10 +0900

    필드와 컬럼 매핑 - 필드를 매핑하는 어노테이션 학습 - 컬럼을 매핑하는 column annotation 내 option 학습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,5 @@
 package hellojpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,19 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-            Member findMember = em.find(Member.class, 150L);
-            findMember.setName("AAAAA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUserName("C");
+            member.setRoleType(RoleType.GUEST);
 
-            // 준영속 => findMember 객체를 준영속 상태로 만들었으므로 update query 가 날라가지 않는다.
-//            em.detach(findMember);
-//            System.out.println("===========");
-
-            // 준영속 => 영속성 컨텍스트를 통으로 날렸으므로 이때도 update query 가 날라가지 않는다.
-            em.clear();
-            Member fineMember2 = em.find(Member.class, 150L);
-            System.out.println("fineMember2.id = " + fineMember2.getId());
-            System.out.println("fineMember2.name = " + fineMember2.getName());
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,20 +1,48 @@
 package hellojpa;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String userName;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob // varchar 보다 큰 문자열을 표현할 때 사용
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Member() {
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -25,11 +53,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
