4c0a1416e7b4c8a25be687c31a058ce744b5c0a1
hajs <dev.hajs@gmail.com>
2021-07-14 19:37:58 +0900

    영속성 전이(CASCADE)와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,34 +18,24 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUserName("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUserName("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
+            em.persist(parent);
+            // Member @OneToMany 에 `cascade = CascadeType.ALL` 속성읇 부여함으로써 해결
+//            em.persist(child1);
+//            em.persist(child2);
 
             em.flush();
             em.clear();
 
-            List<Member> members = em.createQuery("SELECT m FROM Member m join fetch m.team", Member.class).getResultList();
-
-//            Member m = em.find(Member.class, member1.getId());
-//            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-//            System.out.println(">>>>>>>>>>>>>>");
-//            System.out.println("m.getTeam().getName() = " + m.getTeam().getName());
-//            System.out.println(">>>>>>>>>>>>>>");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
+//            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
