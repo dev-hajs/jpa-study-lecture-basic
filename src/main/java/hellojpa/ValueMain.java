a9feb943b33985000f103f0b13ab9884094c2d6b
hajs <dev.hajs@gmail.com>
2021-07-16 16:55:09 +0900

    값 타입의 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,18 +4,13 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-        // primitive type
-//        int a = 10;
-//        int b = a;
-//        a = 20;
-//        System.out.println("a = " + a); // 20
-//        System.out.println("b = " + b); // 10
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b : " + (a == b));
 
-        // wrapper class
-//        Integer c = new Integer(10);
-//        Integer d = c;
-//        c.setValue(20);
-//        System.out.println("c = " + c); // 20
-//        System.out.println("d = " + d); // 20
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 : " + (address1.equals(address2)));
     }
 }
